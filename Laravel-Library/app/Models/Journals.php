a097351fd4b953c3dc9991f416cf1fed63f75de1
Aaron Jevon Benedict Kongdoh <abenedict01@student.ciputra.ac.id>
2024-10-31 09:36:46 +0800

    minor update

---
+++ Laravel-Library/app/Models/Journals.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Journals extends Model
+{
+    use HasFactory;
+    //
+    protected $fillable = [
+        'title',
+        'author',
+        'publisher',
+        'publicationYear',
+    ];
+}
